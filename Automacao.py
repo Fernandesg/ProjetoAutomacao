fe79ca3d63157413ffa196e573e916e9770b9361
fernandesg <guilherme.fnds@hotmail.com>
2022-08-16 18:26:58 -0300

    Adicionado barra de progresso e mensagens

--- Automacao.py
+++ Automacao.py
@@ -3,6 +3,7 @@ import time
 import PySimpleGUI as sg
 from playwright.sync_api import sync_playwright
 from passwords import *
+import sys
 
 today = date.today()
 
@@ -17,13 +18,14 @@ layout = [
     [sg.Text('      Selecione a filial'), sg.Push()],
     [sg.Push(), sg.Combo(['75 - VERO SANTO ANTONIO DA PATRULHA','86 - VERO SANTO ANTONIO DA PATRULHA II'], key='filial', size=(43,1)), sg.Push()],
     [sg.Push(), sg.Button('Criar requisição', size=(17, 1)), sg.Push(), sg.Button('Cancelar', size=(17, 1)), sg.Push()],
-    [sg.Push(), sg.Text('', key='mensagem1'), sg.Push(),],
+    [sg.Push(), sg.ProgressBar(max_value=8, orientation='h', size=(20, 20), key='progress', visible=False),sg.Push(), sg.Text('', visible=False, key='espacamento',size=(8,1))],
+    [sg.Push(), sg.Text('', key='mensagemProgresso'), sg.Push(),],
     [sg.Push(), sg.Text('', key='mensagem2'), sg.Push(),],
-    [sg.Push(), sg.Text('', key='mensagem3'), sg.Push(), sg.Input('', key='mensagem4',size=(18,1),disabled=True, visible=False), sg.Push()]
+    [sg.Push(), sg.Text('', key='mensagem3'), sg.Push(), sg.Input('', key='mensagem4',size=(18,1),disabled=True, visible=False), sg.Push(), sg.Text('', visible=False, key='espacamento2',size=(8,1))]
     ]
 
 window = sg.Window('Abertura de requisições', size=(400, 400), layout = layout)
-
+progress_bar = window['progress']
 
 while True:
     
@@ -41,22 +43,27 @@ while True:
             quant = str(values['quant'])
             data_esperada = values['data_esperada']
             filial = values['filial']
-
+            progress_bar.update(visible = True)
+            window['espacamento'].update(visible = True)
 
             with sync_playwright() as p:
                 valorTotal = str(float(valorun) * int(quant))
-                browser = p.chromium.launch(headless=False)
+                browser = p.chromium.launch()
                 page = browser.new_page()
                 page.goto(site)
+                progress_bar.update(visible = True)
+                window['espacamento'].update(visible = True)
 
                 # LOGIN ME
-
+                window['mensagemProgresso'].update('Efetuando login no ME')      
+                progress_bar.update_bar(1) 
                 page.locator('xpath=//*[@id="LoginName"]').fill(usuario_me)
                 page.locator('xpath=//*[@id="RAWSenha"]').fill(senha_me)
                 page.locator('xpath=//*[@id="SubmitAuth"]').click()
 
                 # CONFIGURAÇÃO DA REQUISIÇÃO
-
+                window['mensagemProgresso'].update('Configurando a requisição')      
+                progress_bar.update_bar(2)
                 page.locator('xpath=//*[@id="__layout"]/div/main/div/div/div[2]/div/div[1]/section/div/div/div/button').click()
                 page.locator('xpath=//*[@id="__layout"]/div/main/div/div/div[2]/div/div[1]/section/div[1]/div[2]/ul/li[1]/a').click()
                 page.locator('xpath=//*[@id="__layout"]/div/main/div/div/div[2]/div/div[1]/section/div[1]/div[2]/ul[2]/li[1]/a').click()
@@ -68,7 +75,8 @@ while True:
                 frame.locator('//*[@id="btnSave"]').click()
 
                 # SELECIONA ITENS E QUANTIDADES
-
+                window['mensagemProgresso'].update('Adicionando itens e quantidades')      
+                progress_bar.update_bar(3)
                 page.locator('xpath=//*[@id="Valor"]').fill(item)
                 page.locator('xpath=//*[@id="btnSearchSimple"]').click()
                 page.locator('xpath=//*[@id="149419566"]').click()
@@ -76,11 +84,12 @@ while True:
                 page.locator('//*[@id="input_qtde_149419566"]').fill(quant)
                 page.wait_for_timeout(1000)
                 page.locator('//*[@id="input_qtde_149419566"]').press('Tab')
-                page.wait_for_timeout(1000)
+                page.wait_for_timeout(2000)
                 page.locator('xpath=//*[@id="btnAvancar"]').click()
 
                 # TELA CONDIÇÕES GERAIS
-
+                window['mensagemProgresso'].update('Ajustando condições gerais')      
+                progress_bar.update_bar(4)
                 page.locator('xpath=//*[@id="Titulo_Value"]').fill(titulo_requisicao)
                 page.locator('xpath=//*[@id="select2-LocalEntrega_Value-container"]').click()
                 page.locator('xpath=/html/body/span/span/span[1]/input').fill(filial[5:])
@@ -94,7 +103,8 @@ while True:
                 page.locator('xpath=//*[@id="btnAvancar"]').click()
                 
                 #            TELA DETALHES DOS ITENS
-
+                window['mensagemProgresso'].update('Ajustando detalhes dos itens')      
+                progress_bar.update_bar(5)
                 page.locator('xpath=//*[@id="Itens_0__PrecoEstimado_Value"]').fill(valorun.replace(".",","))
                 page.locator('xpath=//*[@id="select2-Itens_0__CategoriaContabil_Value-container"]').click()
                 page.locator('xpath=//*[@id="select2-Itens_0__CategoriaContabil_Value-container"]').press('Enter')
@@ -104,12 +114,15 @@ while True:
                 page.locator('xpath=//*[@id="btnAvancar"]').click()
 
                 # FINALIZAR REQUISIÇÃO
-
+                window['mensagemProgresso'].update('Finalizando a requisição')      
+                progress_bar.update_bar(6)
                 page.locator('xpath=//*[@id="btnAvancar"]').click()
                 requisicao = page.locator('.badge-code ')
-                window['mensagem1'].update('########### REQUISIÇÃO FINALIZADA ###########')
+                page.wait_for_timeout(1000)
+                window['mensagemProgresso'].update('########### REQUISIÇÃO FINALIZADA ###########')
                 window['mensagem2'].update(titulo_requisicao)
                 window['mensagem3'].update('Sua requisição é: ')
                 window['mensagem4'].update(visible=True)
+                window['espacamento2'].update(visible = True)
                 window['mensagem4'].update(requisicao.inner_html().replace(' ',''))
 
