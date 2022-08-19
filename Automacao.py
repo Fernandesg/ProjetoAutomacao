9fdbd98cc37b81092a6af0e36c5339198110b27f
fernandesg <guilherme.fnds@hotmail.com>
2022-08-19 08:40:00 -0300

    Adicionado arquivos para inserir variaveis

--- Automacao.py
+++ Automacao.py
@@ -1,4 +1,5 @@
 from datetime import date
+from pydoc import visiblename
 import PySimpleGUI as sg
 from playwright.sync_api import sync_playwright
 
@@ -12,19 +13,44 @@ usuario_me = login[0][14:-1]
 senha_me = login[1][12:-1]
 site = login[2][8:-1]
 
+filiais_caminho = open('C:/DEV/PYTHON/ProjetoAutomacao/filiais.txt', 'r')
+filiais = []
+
+for linhas in filiais_caminho:
+    linhas = linhas.strip()
+    filiais.append(linhas)
+
+cod_caminho = open('C:/DEV/PYTHON/ProjetoAutomacao/codigos.txt', 'r')
+codigos = []
+
+for linhas in cod_caminho:
+    linhas = linhas.strip()
+    codigos.append(linhas)
+
+cc_caminho = open('C:/DEV/PYTHON/ProjetoAutomacao/centro de custos.txt', 'r')
+centroCustos = []
+
+for linhas in cc_caminho:
+    linhas = linhas.strip()
+    centroCustos.append(linhas)
+
 today = date.today()
 
 layout = [
     [sg.Text('      Título da requisição'), sg.Push()],
     [sg.Push(), sg.Input(key='titulo_requisicao'), sg.Push()],
     [sg.Text('      Item'), sg.Push(), sg.Text('            Valor unitário'), sg.Push()],
-    [sg.Push(), sg.Input('SRVT00190', size=(20, 1), key='item'), sg.Push(), sg.Input(key='valorun', size=(20, 1)), sg.Push()],
+    [sg.Push(), sg.Combo(codigos, size=(18, 1), key='item', default_value='SRVT00190'), sg.Push(), sg.Input(key='valorun', size=(20, 1)), sg.Push()],
     [sg.Text('      Quantidade'), sg.Push(), sg.Text('    Data esperada'), sg.Push(), ],
     [sg.Push(), sg.Input(key='quant', size=(20, 1)), sg.Push(), sg.Input(today.strftime("%d/%m/%Y"), key='data_esperada', size=(20, 1)), sg.Push(), ],
-    [sg.Text('      Selecione a categoria'),sg.Text('                   Centro de custo'), sg.Push()],
-    [sg.Push() ,sg.Combo(['PEDIDO COMPRA PADRÃO','PEDIDO REGULARIZAÇÃO'], key='catPedido', size=(25,1)),sg.Push(),sg.Input('0312', size=(15, 1), key='centrocusto'), sg.Push()],
+    [sg.Text('      Selecione a categoria'),sg.Text('                 Centro de custo'), sg.Push()],
+    [sg.Push() ,sg.Combo(['PEDIDO COMPRA PADRÃO','PEDIDO REGULARIZAÇÃO'], key='catPedido', size=(23,1), readonly=True, default_value='PEDIDO COMPRA PADRÃO'),sg.Combo(centroCustos, size=(16, 1), key='centrocusto', default_value='0312 - Supply Chain Estoque', readonly=True), sg.Push()],
     [sg.Text('      Selecione a filial'), sg.Push()],
-    [sg.Push(), sg.Combo(['75 - VERO SANTO ANTONIO DA PATRULHA','86 - VERO SANTO ANTONIO DA PATRULHA II'], key='filial', size=(43,1)), sg.Push()],
+    [sg.Push(), sg.Combo(filiais, key='filial', size=(43,1), readonly=True, default_value='86 - VERO SANTO ANTONIO DA PATRULHA II'), sg.Push()],
+    [sg.Text('      Comentario:')],
+    [sg.Push(), sg.Input(key='comentario'),sg.Push()],
+    [sg.Text("      Selecionar arquivo: (Apenas pedido de regularização)", key='txtSelecionaArquivo')], 
+    [sg.Push(),sg.Input(size=(35,1)), sg.FileBrowse('Procurar',key="caminhoArquivo"), sg.Push()],
     [sg.Push(), sg.Button('Criar requisição', size=(17, 1)), sg.Push(), sg.Button('Cancelar', size=(17, 1)), sg.Push()],
     [sg.Push(), sg.ProgressBar(max_value=7, orientation='h', size=(20, 20), key='progress', visible=False),sg.Push(), sg.Text('', visible=False, key='espacamento',size=(8,1))],
     [sg.Push(), sg.Text('', key='mensagemProgresso'), sg.Push(),],
@@ -32,19 +58,27 @@ layout = [
     [sg.Push(), sg.Text('', key='mensagem3'), sg.Push(), sg.Input('', key='mensagem4',size=(18,1),disabled=True, visible=False), sg.Push(), sg.Text('', visible=False, key='espacamento2',size=(8,1))]
     ]
 
-window = sg.Window('Abertura de requisições', size=(400, 400), layout = layout)
+window = sg.Window('Abertura de requisições', size=(400, 600), layout = layout)
 progress_bar = window['progress']
 
 while True:
-    
     event, values = window.read()
-    print(event, values)
+
+
     match(event):
         case None:
             break
         case 'Cancelar':
             break
         case 'Criar requisição': 
+            window['mensagem2'].update('')
+            window['mensagem3'].update('')
+            window['mensagem4'].update(visible=False)
+            window['espacamento2'].update(visible = False)
+            window['mensagem4'].update('')
+
+            comentario = values['comentario']
+            caminho_arquivo = values["caminhoArquivo"]
             centro_custo = values['centrocusto']
             cat_Pedido = values['catPedido']
             titulo_requisicao = values['titulo_requisicao']
@@ -98,12 +132,12 @@ while True:
                 page.wait_for_timeout(1000)
                 page.locator('xpath=//*[@id="Valor"]').fill(item)
                 page.locator('xpath=//*[@id="btnSearchSimple"]').click()
-                page.locator('xpath=//*[@id="149419566"]').click()
-                page.wait_for_timeout(2000)
-                page.locator('//*[@id="input_qtde_149419566"]').fill(quant)
+                page.locator('.icon-shopping-cart').click()
                 page.wait_for_timeout(1000)
-                page.locator('//*[@id="input_qtde_149419566"]').press('Tab')
-                page.wait_for_timeout(2000)
+                page.locator('[name=\"item-quantity\"]').fill(quant)
+                page.wait_for_timeout(1000)
+                page.locator('[name=\"item-quantity\"]').press('Tab')
+                page.wait_for_timeout(1000)
                 page.locator('xpath=//*[@id="btnAvancar"]').click()
 
                 # TELA CONDIÇÕES GERAIS
@@ -115,12 +149,13 @@ while True:
                 page.locator('xpath=//*[@id="select2-LocalEntrega_Value-container"]').click()
                 page.locator('xpath=/html/body/span/span/span[1]/input').fill(filial[5:])
                 page.locator('xpath=/html/body/span/span/span[1]/input').press('Enter')
-                page.locator('xpath=//*[@id="CentroCusto_Text"]').fill('0312')
+                page.locator('xpath=//*[@id="CentroCusto_Text"]').fill(centro_custo[:4])
                 page.wait_for_timeout(1000)
                 page.locator('xpath=//*[@id="ui-id-2"]').click()
                 page.locator('xpath=//*[@id="select2-LocalFaturamento_Value-container"]').click()
                 page.locator('xpath=/html/body/span/span/span[1]/input').fill(filial[5:])
                 page.locator('xpath=/html/body/span/span/span[1]/input').press('Enter')
+                page.locator('xpath=//*[@id="Observacao_Value"]').fill(comentario)
                 page.locator('xpath=//*[@id="btnAvancar"]').click()
                 
                 #            TELA DETALHES DOS ITENS
@@ -136,11 +171,22 @@ while True:
                     page.locator('xpath=//*[@id="Itens_0__Attributes_1__valor"]').fill(titulo_requisicao)
                     page.locator('xpath=//*[@id="Itens_0__Attributes_0__valor"]').fill(valorTotal.replace(".",","))
                 page.locator('xpath=//*[@id="btnAvancar"]').click()
-                page.wait_for_timeout(10000)
+                page.wait_for_timeout(1000)
+
 
                 # FINALIZAR REQUISIÇÃO
                 window['mensagemProgresso'].update('Finalizando a requisição')      
                 progress_bar.update_bar(6)
+                if cat_Pedido == 'PEDIDO REGULARIZAÇÃO':
+                    with page.expect_popup() as popup_info:
+                        page.locator('xpath=//*[@id="anexoReq_link"]').click()
+                        popup = popup_info.value
+                        popup.wait_for_load_state()
+                        popup.locator('xpath=//*[@id="fuArquivo"]').set_input_files(caminho_arquivo)
+                        popup.locator('xpath=//*[@id="ctl00_conteudo_formUpload_btn_ctl00_conteudo_formUpload_btnEnviar"]').click()
+                        popup.wait_for_load_state()
+                        popup.close()  
+
                 page.locator('xpath=//*[@id="btnAvancar"]').click()
                 requisicao = page.locator('.badge-code ')
                 page.wait_for_timeout(1000)
@@ -151,4 +197,3 @@ while True:
                 window['mensagem4'].update(visible=True)
                 window['espacamento2'].update(visible = True)
                 window['mensagem4'].update(requisicao.inner_html().replace(' ',''))
-
